548ed98f4f8178b0da10445a7633bf2a6826ec35
jaks97 <jkhalilsilva@gmail.com>
2020-04-23 16:15:26 -0300

    Integrante 4

--- PruebaConcepto/src/main/java/DummyDesign.java
+++ PruebaConcepto/src/main/java/DummyDesign.java
@@ -1,7 +1,11 @@
 
 public class DummyDesign {
-	
+
 	public int integrante5(){
 		return 5;
+	}
+
+	public int integrante4(){
+		return 4;
 	}
 }
--- PruebaConcepto/src/test/java/TestDummyDesign.java
+++ PruebaConcepto/src/test/java/TestDummyDesign.java
@@ -6,10 +6,17 @@ public class TestDummyDesign {
 
 	@Test
 	public void testIntegrante5 () {
-		
+
 		DummyDesign desing = new DummyDesign();
 		int result = desing.integrante5();
 		assertEquals(result,5);
+	}
+
+	@Test
+	public void testIntegrante4 () {
+		DummyDesign design = new DummyDesign();
+		int result = design.integrante4();
+		assertEquals(result,4);
 	}
 
 }
