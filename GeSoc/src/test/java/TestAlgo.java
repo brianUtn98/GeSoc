95d63d81793dd02eae20ba323279cddcc7b7d237
smchejolan <smchejolan@gmail.com>
2020-05-13 18:56:55 -0300

    Agregados test de comportamiento en enum categoria

--- GeSoc/src/test/java/TestAlgo.java
+++ GeSoc/src/test/java/TestAlgo.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import Dominio.*;
 
 import static org.junit.Assert.*;
 
@@ -6,5 +7,12 @@ public class TestAlgo {
     @Test
     public void testPrenda() {
         assertEquals(5, 5);
+    }
+    
+    @Test
+    public void testCategoria() {
+    	assertFalse(CategoriaDeEntidad.OSC.esEmpresa());
+    	assertTrue(CategoriaDeEntidad.Micro.esEmpresa());
+    	assertTrue(CategoriaDeEntidad.MedianaTramo2.esEmpresa());
     }
 }
